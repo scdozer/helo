52bc2d10c752f5f92d39c09f5839a782bb739020
Scott Cardoza <Scott@Scotts-MacBook-Pro.local>
2017-11-09 22:18:56 -0700

    first commit

---
+++ server/auth0strategy.js
@@ -0,0 +1,19 @@
+const Auth0Strategy = require('passport-auth0');
+const config = require('./config');
+let app = require('./index');
+console.log('app', app);
+module.exports = new Auth0Strategy({
+  domain:       config.domain,
+  clientID:     config.clientId,
+  clientSecret: config.clientSecret,
+  callbackURL:  config.callback,
+  scope: 'openid email profile'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    console.log('profile', profile);
+    return done(null, profile);
+  }
+);
---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  domain: 'scottcardoza.auth0.com',
+  clientId: 'bnBjZCqQ-9rKIg8B2GvuLG3_jY_gHijE',
+  clientSecret: 'JUkcOqG1LG7ydcvaPlUxF0936-l0rgqdzv5r_9tmg1Fh5R0jHrhrajzZg_OYbICr',
+  callback: 'http://localhost:3005/auth/callback'
+}
